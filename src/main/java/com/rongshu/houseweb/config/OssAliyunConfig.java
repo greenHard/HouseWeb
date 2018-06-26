7d51b9735c24013da656d52eeecd516a542509b7
greenHard <m13047177942@163.com>
2018-06-26 09:27:10 +0800

    各种修改

---
+++ src/main/java/com/rongshu/houseweb/config/OssAliyunConfig.java
@@ -0,0 +1,33 @@
+package com.rongshu.houseweb.config;
+
+import com.rongshu.houseweb.entity.OssAliyunField;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 阿里云配置类
+ * @author: Peter
+ * @date: 2018-4-11
+ */
+@Configuration
+public class OssAliyunConfig {
+    //
+    //@Bean(value="defaultOssAliyunField")
+    //@ConfigurationProperties("oss.aliyun.defalut")
+    //public OssAliyunField defaultOssAliyunField() {
+    //    return new OssAliyunField();
+    //}
+
+    @Bean(value="firstOssAliyuField")
+    @ConfigurationProperties("oss.aliyun.first")
+    public OssAliyunField firstOssAliyuField() {
+        return new OssAliyunField();
+    }
+
+    //@Bean(value = "secondOssAliyuField")
+    //@ConfigurationProperties("oss.aliyun.second")
+    //public OssAliyunField secondOssAliyuField() {
+    //    return new OssAliyunField();
+    //}
+}
